e643625576c203f8b7696be7b61820f5a0c60f7d
supuni2006 <supuni2006k@gmail.com>
2026-05-12 17:20:52 +0530

    Prune deps and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [
+    react(),
+  ],
+
   tanstackStart: {
     server: {
-      entry: "server",
+      entry: "./server.ts",
     },
   },
 
